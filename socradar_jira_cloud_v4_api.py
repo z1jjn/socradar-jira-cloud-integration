1e194ee3d259e7675b830e8c2a5378069c044f19
Johanne Lee <152343706+z1jjn@users.noreply.github.com>
2024-10-18 08:54:27 +0800

    Update socradar_jira_cloud_v4_api.py
    implemented rale limiter to prevent rate limit exceed error

--- socradar_jira_cloud_v4_api.py
+++ socradar_jira_cloud_v4_api.py
@@ -3,6 +3,9 @@ from atlassian import Jira
 import re
 from datetime import datetime
 import asyncio
+from aiolimiter import AsyncLimiter
+
+rate_limit = AsyncLimiter(1, 1.5)
 
 socradar_company_id = ''
 socradar_api_key = ''
@@ -17,6 +20,7 @@ alarm_main_type_field_id = 'customfield_'
 alarm_sub_type_field_id = 'customfield_'
 project_key = 'soc'
 reporter_account_id = ''
+default_assignee = ''
 
 jira = Jira(
     url='https://instance.atlassian.net',
@@ -38,91 +42,95 @@ async def create_issue():
     count = 1
     while True:
         print(f"Creating issues from page: {count}.")
-        response = requests.get(f"{socradar_api_url}{socradar_company_id}/incidents/{socradar_api_version}?key={socradar_api_key}&is_resolved=0&is_false_positive=0&limit=100&page={count}")
+        async with rate_limit:
+            response = requests.get(f"{socradar_api_url}{socradar_company_id}/incidents/{socradar_api_version}?key={socradar_api_key}&is_resolved=0&is_false_positive=0&limit=100&page={count}")
         if response.status_code == 200:
             if len(response.json().get('data', [])) == 0:
                 break
             data = response.json()
             for value in data.get('data', []):
-                alarm_assignee = value.get('alarm_assignees', [])
-                first_assignee = alarm_assignee[0] if alarm_assignee else "jsapalo@ictsi.com"
-                alarm_summary = value.get('alarm_type_details').get('alarm_generic_title')
-                alarm_id = value.get('alarm_id')
-                alarm_main_type = value.get('alarm_type_details').get('alarm_main_type')
-                alarm_sub_type = value.get('alarm_type_details').get('alarm_sub_type')
-                alarm_text = value.get('alarm_text')
-                parsed_text = f'{alarm_text[:32000]} ... (exceeds the character limit)' if len(alarm_text) > 32000 else alarm_text
-                alarm_risk_level = value.get('alarm_risk_level')
-                alarm_response = value.get('alarm_response')
-                alarm_default_mitigation_plan = value.get('alarm_type_details').get('alarm_default_mitigation_plan')
-                soc_issue = {
-                    'project': {'key': {project_key}},
-                    'summary': f"[{str(alarm_id)}] {alarm_summary}",
-                    'description': parsed_text
-                    + "\n\n"
-                    + "Direct Link: "
-                    + f"https://platform.socradar.com/app/company/{socradar_company_id}/alarm-management?tab=approved&alarmId={alarm_id}",
-                    'issuetype': {'name': 'Incident'},
-                    request_type_field_id: request_type,
-                    'assignee': {
-                        'accountId': jira.user_find_by_user_string(
-                            query=first_assignee,
-                            start=0,
-                            limit=1,
-                            include_inactive_users=False,
-                        )[0].get('accountId')
-                    },
-                    alarm_response_field_id: add_newline_before_number(
-                        alarm_response
-                    ),
-                    alarm_default_mitigation_plan_field_id: add_newline_before_number(
-                        alarm_default_mitigation_plan
-                    ),
-                    'reporter': {
-                        'accountId': {reporter_account_id}
-                    },
-                    'priority': {'name': str(alarm_risk_level).title()},
-                    alarm_main_type_field_id: alarm_main_type,
-                    alarm_sub_type_field_id: alarm_sub_type,
-                }
-                if check_existence(alarm_id) == 0:
-                    try:
-                        jira.issue_create(soc_issue)
-                    except requests.exceptions.HTTPError as err:
-                        if "cannot be assigned issues" in str(err):
-                            soc_issue = {
-                                'project': {'key': {project_key}},
-                                'summary': f"[{str(alarm_id)}] {alarm_summary}",
-                                'description': parsed_text
-                                + "\n\n"
-                                + "Direct Link: "
-                                + f"https://platform.socradar.com/app/company/{socradar_company_id}/alarm-management?tab=approved&alarmId={alarm_id}",
-                                'issuetype': {'name': 'Incident'},
-                                request_type_field_id: request_type,
-                                'assignee': {
-                                    'accountId': jira.user_find_by_user_string(
-                                        query=first_assignee,
-                                        start=0,
-                                        limit=1,
-                                        include_inactive_users=False,
-                                    )[0].get('accountId')
-                                },
-                                alarm_response_field_id: add_newline_before_number(
-                                    alarm_response
-                                ),
-                                alarm_default_mitigation_plan_field_id: add_newline_before_number(
-                                    alarm_default_mitigation_plan
-                                ),
-                                'reporter': {
-                                    'accountId': {reporter_account_id}
-                                },
-                                'priority': {'name': str(alarm_risk_level).title()},
-                                alarm_main_type_field_id: alarm_main_type,
-                                alarm_sub_type_field_id: alarm_sub_type,
-                            }
+                async with rate_limit:
+                    alarm_assignee = value.get('alarm_assignees', [])
+                    first_assignee = alarm_assignee[0] if alarm_assignee else default_assignee
+                    alarm_summary = value.get('alarm_type_details').get('alarm_generic_title')
+                    alarm_id = value.get('alarm_id')
+                    alarm_main_type = value.get('alarm_type_details').get('alarm_main_type')
+                    alarm_sub_type = value.get('alarm_type_details').get('alarm_sub_type')
+                    alarm_text = value.get('alarm_text')
+                    parsed_text = f'{alarm_text[:32000]} ... (exceeds the character limit)' if len(alarm_text) > 32000 else alarm_text
+                    alarm_risk_level = value.get('alarm_risk_level')
+                    alarm_response = value.get('alarm_response')
+                    alarm_default_mitigation_plan = value.get('alarm_type_details').get('alarm_default_mitigation_plan')
+                    soc_issue = {
+                        'project': {'key': {project_key}},
+                        'summary': f"[{str(alarm_id)}] {alarm_summary}",
+                        'description': parsed_text
+                        + "\n\n"
+                        + "Direct Link: "
+                        + f"https://platform.socradar.com/app/company/{socradar_company_id}/alarm-management?tab=approved&alarmId={alarm_id}",
+                        'issuetype': {'name': 'Incident'},
+                        request_type_field_id: request_type,
+                        'assignee': {
+                            'accountId': jira.user_find_by_user_string(
+                                query=first_assignee,
+                                start=0,
+                                limit=1,
+                                include_inactive_users=False,
+                            )[0].get('accountId')
+                        },
+                        alarm_response_field_id: add_newline_before_number(
+                            alarm_response
+                        ),
+                        alarm_default_mitigation_plan_field_id: add_newline_before_number(
+                            alarm_default_mitigation_plan
+                        ),
+                        'reporter': {
+                            'accountId': {reporter_account_id}
+                        },
+                        'priority': {'name': str(alarm_risk_level).title()},
+                        alarm_main_type_field_id: alarm_main_type,
+                        alarm_sub_type_field_id: alarm_sub_type,
+                    }
+                    if check_existence(alarm_id) == 0:
+                        try:
+                            await asyncio.sleep(0)
                             jira.issue_create(soc_issue)
-                        else:
-                            print(f"{err} at alarm id {str(alarm_id)}")
+                        except requests.exceptions.HTTPError as err:
+                            if "cannot be assigned issues" in str(err):
+                                soc_issue = {
+                                    'project': {'key': {project_key}},
+                                    'summary': f"[{str(alarm_id)}] {alarm_summary}",
+                                    'description': parsed_text
+                                    + "\n\n"
+                                    + "Direct Link: "
+                                    + f"https://platform.socradar.com/app/company/{socradar_company_id}/alarm-management?tab=approved&alarmId={alarm_id}",
+                                    'issuetype': {'name': 'Incident'},
+                                    request_type_field_id: request_type,
+                                    'assignee': {
+                                        'accountId': jira.user_find_by_user_string(
+                                            query=first_assignee,
+                                            start=0,
+                                            limit=1,
+                                            include_inactive_users=False,
+                                        )[0].get('accountId')
+                                    },
+                                    alarm_response_field_id: add_newline_before_number(
+                                        alarm_response
+                                    ),
+                                    alarm_default_mitigation_plan_field_id: add_newline_before_number(
+                                        alarm_default_mitigation_plan
+                                    ),
+                                    'reporter': {
+                                        'accountId': {reporter_account_id}
+                                    },
+                                    'priority': {'name': str(alarm_risk_level).title()},
+                                    alarm_main_type_field_id: alarm_main_type,
+                                    alarm_sub_type_field_id: alarm_sub_type,
+                                }
+                                await asyncio.sleep(0)
+                                jira.issue_create(soc_issue)
+                            else:
+                                print(f"{err} at alarm id {str(alarm_id)}")
             count += 1
         else:
             print(f"Failed to retrieve data. Status code: {response.status_code}")
@@ -130,24 +138,31 @@ async def create_issue():
 async def update_tickets(query, change_status):
     jql_request = query
     pattern = r'\[(.*?)\]'
-    for issue in jira.jql(jql_request)['issues']:
-        comments = jira.issue_get_comments(issue.get('key'))
-        comment_text = "".join(
-            f"""{comment.get('author').get('emailAddress')} commented at {datetime.strptime(comment.get('created'), "%Y-%m-%dT%H:%M:%S.%f%z")}: {comment.get('body')}. """
-            for comment in comments.get('comments')
-        )
-        summary = issue.get('fields').get('summary')
-        status = str(issue.get('fields').get('status').get('name')).upper()
-        alarm_ids = re.findall(pattern, summary)
-        for alarm_id in alarm_ids:
-            check_status = requests.get(f"{socradar_api_url}{socradar_company_id}/incidents/{socradar_api_version}?key={socradar_api_key}&alarm_ids={alarm_id}")
-            if check_status.status_code == 200:
-                for alarm_status in check_status.json().get('data'):
-                    if status != alarm_status.get('status'):
-                        headers = { "Content-Type": "application/json", "API-KEY": socradar_api_key }
-                        payload = { "alarm_ids": alarm_id, "status": change_status, "comments": comment_text }
-                        response = requests.post(f"{socradar_api_url}{socradar_company_id}/alarms/status/change", json = payload, headers = headers)
-                        print(response.text)
+
+    async with rate_limit:
+        issues = jira.jql(jql_request)['issues']
+
+    for issue in issues:
+        async with rate_limit:
+            comments = jira.issue_get_comments(issue.get('key'))
+            comment_text = "".join(
+                f"""{comment.get('author').get('emailAddress')} commented at {datetime.strptime(comment.get('created'), "%Y-%m-%dT%H:%M:%S.%f%z")}: {comment.get('body')}. """
+                for comment in comments.get('comments')
+            )
+            summary = issue.get('fields').get('summary')
+            status = str(issue.get('fields').get('status').get('name')).upper()
+            alarm_ids = re.findall(pattern, summary)
+            for alarm_id in alarm_ids:
+                async with rate_limit:
+                    check_status = requests.get(f"{socradar_api_url}{socradar_company_id}/incidents/{socradar_api_version}?key={socradar_api_key}&alarm_ids={alarm_id}")
+                    if check_status.status_code == 200:
+                        for alarm_status in check_status.json().get('data'):
+                            if status != alarm_status.get('status'):
+                                headers = { "Content-Type": "application/json", "API-KEY": socradar_api_key }
+                                payload = { "alarm_ids": alarm_id, "status": change_status, "comments": comment_text }
+                                async with rate_limit:
+                                    response = requests.post(f"{socradar_api_url}{socradar_company_id}/alarms/status/change", json = payload, headers = headers)
+                                    print(response.text)
 
 async def main():
     await asyncio.gather(
